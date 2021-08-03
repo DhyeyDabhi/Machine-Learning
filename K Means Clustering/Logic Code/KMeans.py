8f88a4c43ac1974b66d74d221db72bc6751721da
DhyeyDabhi <65999154+DhyeyDabhi@users.noreply.github.com>
2021-08-03 11:25:43 +0530

    Comments for K Means Clustering Added!!

--- K Means Clustering/Logic Code/KMeans.py
+++ K Means Clustering/Logic Code/KMeans.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 x= ""
 
 def calc_color_indxs(centroids):
+    #function assigns centroid indexes to each training example i.e. it assigns the 
+    # nearest cluster centroid to each training example
+    # It uses Eucledian Distance to measure the distance between cluster centroids and training example
     global x
     centroid_indx = np.zeros(((x.shape[0]),1))
     for i in range(0,x.shape[0]):
@@ -16,12 +19,16 @@ def calc_color_indxs(centroids):
     return centroid_indx.astype(int)
 
 def calc_cost(centroids,sample_color_indx):
+    #calculates the cost value of the calculated centroid.
+    #cost = average of the distances between the centroids and the assigned training examples
     sample_centroids = centroids[sample_color_indx.reshape((sample_color_indx.shape[0]))]
     dist = x - sample_centroids
     dist = np.sum(np.power(np.sum(np.power(dist,2),axis = 1),0.5),axis = 0)
     return dist/sample_centroids.shape[0]
 
 def update_centroids(centroids,sample_color_indx,k):
+    #updates the centroid for each assigned cluster
+    #calculates the centroid by taking mean of all the example assigned to the cluster
     for i in range(0,k):
         indxs = np.where(sample_color_indx == i)
         x_centroid = x[indxs[0]]
@@ -60,6 +67,9 @@ if __name__ == '__main__':
         sample_color_indx = calc_color_indxs(centroids)
         cost1 = calc_cost(centroids,sample_color_indx)
     print(cost0)
+    #plots two subplots in a figure,
+    #1.) Cost funcn vs. no. of iterations 
+    #2.) Plot Training examples of same clusters with same color.
     plt.subplot(1,2,2)
     sample_color_indx = calc_color_indxs(prev_centroids)
     colors = plt.cm.Spectral(np.linspace(0,1,k))
